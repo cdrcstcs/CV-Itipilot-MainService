3e88c30cc8cd8cde4863bdec8df9452ec39cd07a
hieu <hieushinnosuke@gmail.com>
2024-06-18 16:46:58 +0800

    add

--- frontend/src/Cookies.jsx
+++ frontend/src/Cookies.jsx
@@ -1,10 +1,6 @@
 import React, { createContext, useContext } from 'react';
 import { Cookies } from 'react-cookie';
-
-// Create a context for the cookies
 const CookiesContext = createContext();
-
-// Create a context provider to wrap your application
 export default function CookiesProvider({ children }) {
   const cookies = new Cookies();
   return (
@@ -13,8 +9,6 @@ export default function CookiesProvider({ children }) {
     </CookiesContext.Provider>
   );
 }
-
-// Custom hook to access the cookies instance
 export function useCookies() {
   return useContext(CookiesContext);
 }
